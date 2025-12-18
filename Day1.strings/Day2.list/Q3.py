fbfeb51c69df28d7dfb52c7936bea3fe12b8422d
Akhileshh23 <gondaakhilesh10@gmail.com>
2025-12-18 17:21:09 +0530

    add day2

---
+++ Day1.strings/Day2.list/Q3.py
@@ -0,0 +1,14 @@
+'''Write a program to sum a list with 4 numbers.'''
+
+sums = []
+
+f1 = int(input("Enter no here: "))
+sums.append(f1)
+f2 = int(input("Enter no here: "))
+sums.append(f2)
+f3 = int(input("Enter no here: "))
+sums.append(f3)
+f4 = int(input("Enter no here: "))
+sums.append(f4)
+
+print(sum(sums))
