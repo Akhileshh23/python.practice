8c110c726a7784498a77a4f54a644a0c5d25d14c
Akhileshh23 <gondaakhilesh10@gmail.com>
2025-12-21 17:19:53 +0530

    day5 completed

--- Day5.loops/Q6.py
+++ Day5.loops/Q6.py
@@ -3,9 +3,8 @@
 n = int(input("Enter the number: "))
 
 for i in range(2 , n):
-    
-    if(n%i) == 0:
-        print("Number is not prime")
-        
-    else:
-        print("Number is prime")
+ if(n%i) == 0:
+  print("Number is not prime")
+  break  
+else:
+ print("Number is prime")
